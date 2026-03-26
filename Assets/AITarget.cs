2de959986991ee17266860617e608547fdf20ae8
RobertChen122 <577089468@qq.com>
2026-03-26 02:37:37 -0400

    Add different stage for the monster

--- Assets/AITarget.cs
+++ Assets/AITarget.cs
@@ -5,16 +5,21 @@ public class AITarget : MonoBehaviour
 {
     public Transform target;
     public float AttackDistance;
+    public int curStage;
     
     private NavMeshAgent m_agent;
-    private Animator m_animator;
+    //private Animator m_animator;
     private float m_distance;
+    private bool m_stageChanged;
+    private float m_speed_s1 = 5f;
+    private float m_speed_s2 = 10f;
+    private float m_speed_s3 = 15f;
     
     // Start is called before the first frame update
     void Start()
     {
         m_agent = GetComponent<NavMeshAgent>();
-        m_animator = GetComponent<Animator>();
+        //m_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -33,9 +38,17 @@ public class AITarget : MonoBehaviour
             m_agent.SetDestination(target.position);
         }
 
-        // void OnAnimatiorMove()
-        // {
-        //     
-        // }
+        if(m_stageChanged)
+            CheckStage();
+    }
+
+    void CheckStage()
+    {
+        if (curStage == 1)
+            m_agent.speed = m_speed_s1;
+        else if (curStage == 2)
+            m_agent.speed = m_speed_s2;
+        else if (curStage == 3)
+            m_agent.speed = m_speed_s3;
     }
 }
